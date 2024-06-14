d2b8fe977c53607c98bf052d492d3cc4febf4db6
jscholtz737 <70411756+jscholtz737@users.noreply.github.com>
2024-06-14 08:37:08 -0700

    Add files via upload

---
+++ events.swift
@@ -0,0 +1,30 @@
+[
+    {
+        "name": "A Festival",
+        "location": "Lake Geneva",
+        "description": "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.",
+        "time": "8am-10pm",
+        "imageName": "festival"
+    },
+    {
+        "name": "Lakefest",
+        "location": "Fontana",
+        "description": "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.",
+        "time": "10am-1pm",
+        "imageName": "lake"
+    },
+    {
+        "name": "Farmers Market",
+        "location": "Williams Bay",
+        "description": "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.",
+        "time": "10:30am-10:30pm",
+        "imageName": "festival"
+    },
+    {
+        "name": "Venetian Fest",
+        "location": "Lake Geneva",
+        "description": "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.",
+        "time": "9:30am-5pm",
+        "imageName": "lake"
+    }
+]
